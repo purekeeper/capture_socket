22f131099048e1008ea8f1d0abb37aa2a1cc6016
yangjian.yangjian <yangjian.yangjian@alibaba-inc.com>
2018-07-29 16:51:30 +0800

    fix

--- Example/server.cpp
+++ Example/server.cpp
@@ -5,22 +5,19 @@ TCPServer tcp;
 
 int main()
 {
-
-
-//从配置文件读取监听端口号
-FILE *file;
-  char buf[100];
-                          file = fopen("config", "rb");
-                                memset(buf, 0, sizeof(buf));
-                              fread(buf, sizeof(char), sizeof(buf), file);
-                              int  port=atoi(buf);
-                              cout<<"port"<<port<<endl;
+    //从配置文件读取监听端口号
+    FILE *file;
+    char buf[100];
+    file = fopen("config", "rb");
+    memset(buf, 0, sizeof(buf));
+    fread(buf, sizeof(char), sizeof(buf), file);
+    int port = atoi(buf);
+    cout << "监听port" << port << endl;
+    //启动截图
     tcp.createframegrabber();
+    //绑定
     tcp.setup(port);
+    //监听
     tcp.receive();
-    // if( pthread_create(&msg, NULL, loop, (void *)0) == 0)
-    // {
-    // 	tcp.receive();
-    // }
     return 0;
 }
--- src/TCPServer.cpp
+++ src/TCPServer.cpp
@@ -1,6 +1,6 @@
 #include "TCPServer.h"
 #include "ScreenCapture.h"
-#include "internal/SCCommon.h" //DONT USE THIS HEADER IN PRODUCTION CODE!!!! ITS INTERNAL FOR A REASON IT WILL CHANGE!!! ITS HERE FOR TESTS ONLY!!!
+#include "internal/SCCommon.h"
 #include <algorithm>
 #include <atomic>
 #include <chrono>
@@ -12,8 +12,6 @@
 #include <thread>
 #include <vector>
 
-// THESE LIBRARIES ARE HERE FOR CONVINIENCE!! They are SLOW and ONLY USED FOR
-// HOW THE LIBRARY WORKS!
 #define TJE_IMPLEMENTATION
 #include "tiny_jpeg.h"
 #define LODEPNG_COMPILE_PNG
@@ -33,7 +31,7 @@ void ExtractAndConvertToRGBA(const SL::Screen_Capture::Image &img, unsigned char
             *imgdist++ = imgsrc->R;
             *imgdist++ = imgsrc->G;
             *imgdist++ = imgsrc->B;
-            *imgdist++ = 0; // alpha should be zero
+            *imgdist++ = 0;
             imgsrc++;
         }
         imgsrc = SL::Screen_Capture::GotoNextRow(img, startimgsrc);
@@ -42,18 +40,16 @@ void ExtractAndConvertToRGBA(const SL::Screen_Capture::Image &img, unsigned char
 
 using namespace std::chrono_literals;
 std::shared_ptr<SL::Screen_Capture::IScreenCaptureManager> framgrabber;
-std::atomic<int> realcounter;
-std::atomic<int> onNewFramecounter;
 
-inline std::ostream &operator<<(std::ostream &os, const SL::Screen_Capture::ImageRect &p)
-{
-    return os << "left=" << p.left << " top=" << p.top << " right=" << p.right << " bottom=" << p.bottom;
-}
-inline std::ostream &operator<<(std::ostream &os, const SL::Screen_Capture::Monitor &p)
-{
-    return os << "Id=" << p.Id << " Index=" << p.Index << " Height=" << p.Height << " Width=" << p.Width << " OffsetX=" << p.OffsetX
-              << " OffsetY=" << p.OffsetY << " Name=" << p.Name;
-}
+// inline std::ostream &operator<<(std::ostream &os, const SL::Screen_Capture::ImageRect &p)
+// {
+//     return os << "left=" << p.left << " top=" << p.top << " right=" << p.right << " bottom=" << p.bottom;
+// }
+// inline std::ostream &operator<<(std::ostream &os, const SL::Screen_Capture::Monitor &p)
+// {
+//     return os << "Id=" << p.Id << " Index=" << p.Index << " Height=" << p.Height << " Width=" << p.Width << " OffsetX=" << p.OffsetX
+//               << " OffsetY=" << p.OffsetY << " Name=" << p.Name;
+// }
 
 auto onNewFramestart = std::chrono::high_resolution_clock::now();
 size_t getSize(string path)
@@ -69,96 +65,65 @@ size_t getSize(string path)
     return sizeOfPic;
 }
 
-void sendFile(int newsockfd){
-FILE *file;
- cout << "fopen!!!" << endl;
- //file = fopen("capture.jpg", "rb");
- while(true){
-   char buf[1024*1000];
-                          file = fopen("capture.jpg", "rb");
-                                memset(buf, 0, sizeof(buf));
-                              size_t readlen = fread(buf, sizeof(char), sizeof(buf), file);
-                              cout<<"length"<<readlen<<endl;
-                              //读取并发送
-                             //发送文件大小
-                              cout<<"1111111"<<endl;
+void sendFile(int newsockfd)
+{
+    FILE *file;
+    cout << "fopen!!!" << endl;
+    while (true) {
+        char buf[1024 * 1000];
+        file = fopen("capture.jpg", "rb");
+        memset(buf, 0, sizeof(buf));
+        size_t readlen = fread(buf, sizeof(char), sizeof(buf), file);
+        cout << "length" << readlen << endl;
+        //发送文件大小
+        char file_size[256];
+        sprintf(file_size, "%d", (int)readlen);
+        cout << "file_size" << file_size << endl;
+        if (send(newsockfd, file_size, strlen(file_size), 0) == -1) {
+            return;
+        }
+        //服务端确认收
+        int sizeRecv_size;
+        char sizeBuf[1024];
+        if ((sizeRecv_size = recv(newsockfd, sizeBuf, 1024, 0)) == -1) {
+            return;
+        }
+        if (send(newsockfd, buf, readlen, 0) == -1) {
+            return;
+        }
+        //  fclose(file);
+        char fileBuff[1024];
 
-                             cout<<"2222222"<<endl;
-                            char file_size[256];
-                            sprintf(file_size, "%d", (int)readlen);
-                            cout<<"file_size"<<file_size<<endl;
-                            if(send(newsockfd, file_size, strlen(file_size), 0)==-1){
-                            return;}
-                             cout<<"777777"<<endl;
-                             //服务端确认收
-                             int sizeRecv_size;
-                             char sizeBuf[1024];
-                            if( (sizeRecv_size = recv(newsockfd, sizeBuf, 1024, 0))==-1){
-                            return;}
-                             cout<<"ack="<<sizeRecv_size<<endl;
-
-                          cout << "begin send file!!!" << endl;
-                          int i=0;
-                          cout << "seding!!!"<<i++ << endl;
-
-                              if(send(newsockfd, buf, readlen, 0)==-1){return;}
-                              cout<<"22222222"<<readlen<<endl;
-                          cout << "over send file!!!" << endl;
-                        //  fclose(file);
-                          char fileBuff[1024];
-                          cout << "receive ack file!!!" << endl;
-
-                         if( recv(newsockfd, fileBuff, 1024, 0)==-1){
-                         return;}
-                          cout<<fileBuff[0]<<endl;
-                          cout << "received ack file!!!" << endl;
-                            fclose(file);
+        if (recv(newsockfd, fileBuff, 1024, 0) == -1) {
+            return;
+        }
+        fclose(file);
+    }
 }
-                          }
-
 
 void TCPServer::createframegrabber()
 {
     pthread_detach(pthread_self());
-
-//cout<<"sock_id_111"<<newsockfd<<endl;
-    realcounter = 0;
-    onNewFramecounter = 0;
     framgrabber = nullptr;
-    framgrabber = SL::Screen_Capture::CreateCaptureConfiguration([]() {
-                      auto mons = SL::Screen_Capture::GetMonitors();
-                      std::cout << "Library is requesting the list of monitors to capture!" << std::endl;
-                      for (auto &m : mons) {
-                          std::cout << m << std::endl;
-                      }
-                      return mons;
-                  })
+    framgrabber = SL::Screen_Capture::CreateCaptureConfiguration([]() { return SL::Screen_Capture::GetMonitors(); })
                       ->onNewFrame([&](const SL::Screen_Capture::Image &img, const SL::Screen_Capture::Monitor &monitor) {
-                          cout << "onNewFrame!!!!";
-                         // auto r = realcounter.fetch_add(1);
                           auto s = std::string("capture.jpg");
                           auto size = Width(img) * Height(img) * sizeof(SL::Screen_Capture::ImageBGRA);
                           auto imgbuffer(std::make_unique<unsigned char[]>(size));
                           ExtractAndConvertToRGBA(img, imgbuffer.get(), size);
-                          cout << "write img!!!" << endl;
                           tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char *)imgbuffer.get());
                       })
                       ->start_capturing();
     ;
-    framgrabber->setFrameChangeInterval(std::chrono::milliseconds(1000));
+    framgrabber->setFrameChangeInterval(std::chrono::milliseconds(500));
 }
 void *TCPServer::Task(void *arg)
 {
-
     cout << "connect success!!!" << endl;
-    int n;
-    int newsockfd = *((int*)arg);
-
+    int newsockfd = *((int *)arg);
     char msg[MAXPACKETSIZE];
     pthread_detach(pthread_self());
-  //  createframegrabber();
-  sendFile(newsockfd);
-  //  std::this_thread::sleep_for(std::chrono::seconds(100000));
+    sendFile(newsockfd);
     return 0;
 }
 
@@ -179,19 +144,19 @@ string TCPServer::receive()
     while (1) {
         socklen_t sosize = sizeof(clientAddress);
         int newsockfd = accept(sockfd, (struct sockaddr *)&clientAddress, &sosize);
-        cout<<"sock"<<newsockfd<<endl;
-       // sockfdList.push_back(newsockfd);
+        cout << "sock" << newsockfd << endl;
         str = inet_ntoa(clientAddress.sin_addr);
-        pthread_create(&serverThread, NULL, &Task, (void*) &newsockfd);
+        pthread_create(&serverThread, NULL, &Task, (void *)&newsockfd);
     }
     return str;
 }
 
 string TCPServer::getMessage() { return Message; }
 
-void TCPServer::Send(string msg) {
- //send(newsockfd, msg.c_str(), msg.length(), 0);
- }
+void TCPServer::Send(string msg)
+{
+    // send(newsockfd, msg.c_str(), msg.length(), 0);
+}
 
 void TCPServer::clean()
 {
@@ -202,5 +167,5 @@ void TCPServer::clean()
 void TCPServer::detach()
 {
     close(sockfd);
-   // close(newsockfd);
+    // close(newsockfd);
 }
